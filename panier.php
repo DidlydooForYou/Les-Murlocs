4ea81572cca0da7597ce09b9ad2bdec094eebe8a
DerekSimms <dereksimms0119@gmail.com>
2026-03-18 11:55:31 -0400

    Ajout du template des elements du panier
    + Gérer h1 pour retirer l'espace blanc

--- panier.php
+++ panier.php
@@ -9,14 +9,25 @@
 ?>
 
 <main class="main">
+    <h1> Panier </h1>
 
-<div class="panier-ElementContainer">
-    <span class="panier-nom"> Épée fuckall :speaking_head: </span>
-    <span class="panier-quantite"> 2 </span>
-    <span class="panier-prix"> 2.00$ </span>
-</div>
+    <div class="panier-Container">
 
-
+        <div class="panier-row">
+            <div class="panier-image-box"> 
+                <img class="panier-image" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZMA41rkfY1mp-2Mrv3kNtSvP4kBIaMCIu0A&s" alt="mama mia"> 
+            </div>
+            <div class="panier-content panier-nom">
+                 Épée fuckall :speaking_head: 
+                </div>
+            <div class="panier-content panier-quantite">
+                <span> + </span><span> 2 </span><span> - </span><span> x </span>
+            </div>
+            <div class="panier-content panier-prix">
+                {nb * prix unitaire}
+            </div>
+        </div>
+    </div>
 
 </main>
 
