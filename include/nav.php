5f9a37aed5b3780f228ad25b5594f221c0a0ad2d
DerekSimms <dereksimms0119@gmail.com>
2026-03-18 09:35:01 -0400

    Menu Principal

---
+++ include/nav.php
@@ -0,0 +1,32 @@
+<nav class="navContainer">
+    <span class="navLinkContainer">
+        <a class="navElement navLink" href="index.php">Vitrine</a>
+    </span>
+
+    <span class="navLinkContainer">
+        <a class="navElement navLink" href="inventaire.php">Inventaire</a>
+    </span>
+
+    <span class="navLinkContainer">
+        <a class="navElement navLink" href="enigma.php">Énigmes</a>
+    </span>
+
+    <span class="navLinkContainer">
+        <a class="navElement navLink" href="revente.php">Revente</a>
+    </span>
+
+    <span class="navLinkContainer">
+        <a class="navElement navLink" href="connexion.php">Connexion</a>
+    </span>
+
+    <div class="navLinkContainer">
+        <div class="dropdown navElement navLink"> 
+            Paramètres
+            <div class="dropdown-content-container">
+                <a href="aide.php" class="dropdown-element navLink"> Aide </a>
+                <a href="profil.php" class="dropdown-element navLink"> Profil </a>
+            </div>
+        </div>
+    </div>
+
+</nav>
