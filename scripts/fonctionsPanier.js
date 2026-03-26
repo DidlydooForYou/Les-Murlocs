94e410932239bec94c029fcb00d25a9c23286491
DerekSimms <dereksimms0119@gmail.com>
2026-03-25 20:42:49 -0400

    Panier : style + début fonctions

---
+++ scripts/fonctionsPanier.js
@@ -0,0 +1,15 @@
+function openPanel(){
+    let panel = document.getElementById("confirmation-panel");
+
+    panel.style.display = "flex";
+}
+
+function closePanel(){
+    let panel = document.getElementById("confirmation-panel");
+
+    panel.style.display = "none";
+}
+
+function changeItemQuantite(value){
+
+}
