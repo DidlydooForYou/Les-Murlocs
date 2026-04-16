f53dac28d6592ea9977011d6138f26e717d518dc
DerekSimms <dereksimms0119@gmail.com>
2026-04-16 14:10:38 -0400

    Refactor - DAL

---
+++ DAL/PanierDAL.php
@@ -0,0 +1,59 @@
+<?php
+class PanierDAL{
+    public static function selectByUser(PDO $connexion, int $idJoueur): array {
+        $sql = "SELECT photoItem, nomItem, i.description, p.Item_idItem as idItem, qtPanier, prixOr, prixArgent, prixBronze
+                FROM Panier p INNER JOIN Item i ON p.Item_idItem = i.idItem
+                WHERE p.JoueursJeu_idJoueur = :idJoueur
+                ORDER BY nomItem";
+
+        $stmt = $connexion->prepare($sql);
+
+        $stmt->bindValue('idJoueur', $idJoueur, PDO::PARAM_INT);
+
+        $stmt->execute();
+
+        $result = $stmt->fetchAll();
+
+        return $result;
+    }
+
+    public static function changeItemQuantite(PDO $connexion, int $idJoueur, int $idItem, int $nouvelleQuantite): bool{
+        $sql = "CALL Modifier_Quantite_Panier(:idJoueur,:idItem,:qtItem)";
+
+        $stmt = $connexion->prepare($sql);
+
+        $stmt->bindValue('idJoueur', $idJoueur, PDO::PARAM_INT);
+        $stmt->bindValue('idItem', $idItem, PDO::PARAM_INT);
+        $stmt->bindValue('qtItem', $nouvelleQuantite, PDO::PARAM_INT);
+
+        $result = $stmt->execute();
+
+        return $result;
+    }
+
+    public static function effacerItem(PDO $connexion, int $idJoueur, int $idItem): bool{
+        $sql = "CALL Enlever_Item_Panier(:idJoueur,:idItem)";
+
+        $stmt = $connexion->prepare($sql);
+
+        $stmt->bindValue('idJoueur', $idJoueur, PDO::PARAM_INT);
+        $stmt->bindValue('idItem', $idItem, PDO::PARAM_INT);
+
+        $result = $stmt->execute();
+
+        return $result;
+    }
+
+    public static function acheterItem(PDO $connexion, int $idJoueur): bool{
+        $sql = "CALL Payer_panier(:idJoueur)";
+
+        $stmt = $connexion->prepare($sql);
+
+        $stmt->bindValue('idJoueur', $idJoueur, PDO::PARAM_INT);
+
+        $result = $stmt->execute();
+
+        return $result;
+    }
+}
+?>
