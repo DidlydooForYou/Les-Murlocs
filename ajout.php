faa0e3e1160200d36d0f1e4f925cb338cb52c56b
DidlydooForYou <victorcotenicholson@gmail.com>
2026-04-22 10:22:16 -0400

    Css et edit reviews

--- ajout.php
+++ ajout.php
@@ -229,6 +229,7 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
 }
 ?>
 
+<link rel="stylesheet" href="public/css/enigma.css">
 <title>DarQuest - Ajout</title>
 
 <?php
@@ -237,117 +238,117 @@ include 'include/nav.php';
 ?>
 
 <main class="main">
+    <div class="enigmaAjout-container">
+        <select name="type" id="type" onchange="Form()" style="margin-top : 4px; margin-left:4px;">
+            <option value="" selected>Choisir un type d'objet : </option>
+            <option value="arme">Armes</option>
+            <option value="armure">Armure</option>
+            <option value="potion">Potion</option>
+            <option value="sort">Sort</option>
+        </select>
+        <form action="ajout.php" method="POST" id="formArme" enctype="multipart/form-data"
+            style="display: none; padding: 4px;">
+            <input type="hidden" name="type" value="formArme">
+            <h2>Ajouter une arme</h2>
+            <label for="nom">Nom : </label>
+            <input type="text" name="nom" id="nom" minlength="1" maxlength="80" required><br>
+            <label for="prix">Prix : </label> en bronze
+            <input type="number" id="prix" name="prix" required min="1" max="5000" value="1"> <br>
+            <label for="description">Description : </label> <br>
+            <textarea name="description" id="description" autofocus cols="40" rows="10" style="resize : none;" required
+                minlength="1" maxlength="80"></textarea><br>
+            <label for="efficacite">Efficacité : </label>
+            <input type="number" id="efficacite" name="efficacite" min="1" max="10" value="1" required> <br>
+            <p>Quel est le genre de l'arme ?</p>
+            <input type="radio" id="uneMain" name="genre" value="une main" checked>
+            <label for="uneMain"> Une main</label>
+            <input type="radio" id="deuxMains" name="genre" value="deux mains">
+            <label for="deuxMains">Deux mains</label> <br>
+            <label for="quantite">Quantité à ajouter : </label>
+            <input type="number" min="1" max="20" value="1" name="quantite" id="quantite" required> <br>
+            <label for="image">Image :</label><br>
+            <input type="hidden" name="MAX_FILE_SIZE" value="50000000">
+            <input type="file" id="image" name="url" accept="image/*"><br>
+            <input type="submit" value="Ajouter">
 
-    <select name="type" id="type" onchange="Form()" style="margin-top : 4px; margin-left:4px;">
-        <option value="" selected>Choisir un type d'objet : </option>
-        <option value="arme">Armes</option>
-        <option value="armure">Armure</option>
-        <option value="potion">Potion</option>
-        <option value="sort">Sort</option>
-    </select>
-    <form action="ajout.php" method="POST" id="formArme" enctype="multipart/form-data"
-        style="display: none; padding: 4px;">
-        <input type="hidden" name="type" value="formArme">
-        <h2>Ajouter une arme</h2>
-        <label for="nom">Nom : </label>
-        <input type="text" name="nom" id="nom" minlength="1" maxlength="80" required><br>
-        <label for="prix">Prix : </label> en bronze
-        <input type="number" id="prix" name="prix" required min="1" max="5000" value="1"> <br>
-        <label for="description">Description : </label> <br>
-        <textarea name="description" id="description" autofocus cols="40" rows="10" style="resize : none;" required
-            minlength="1" maxlength="80"></textarea><br>
-        <label for="efficacite">Efficacité : </label>
-        <input type="number" id="efficacite" name="efficacite" min="1" max="10" value="1" required> <br>
-        <p>Quel est le genre de l'arme ?</p>
-        <input type="radio" id="uneMain" name="genre" value="une main" checked>
-        <label for="uneMain"> Une main</label>
-        <input type="radio" id="deuxMains" name="genre" value="deux mains">
-        <label for="deuxMains">Deux mains</label> <br>
-        <label for="quantite">Quantité à ajouter : </label>
-        <input type="number" min="1" max="20" value="1" name="quantite" id="quantite" required> <br>
-        <label for="image">Image :</label><br>
-        <input type="hidden" name="MAX_FILE_SIZE" value="50000000">
-        <input type="file" id="image" name="url" accept="image/*"><br>
-        <input type="submit" value="Ajouter">
-
-    </form>
-    <form action="ajout.php" method="POST" id="formArmure" style="display: none;padding: 4px;"
-        enctype="multipart/form-data">
-        <input type="hidden" name="type" value="formArmure">
-        <h2>Ajouter une armure</h2>
-        <label for="nom">Nom : </label>
-        <input type="text" name="nom" id="nom" minlength="1" maxlength="80" required><br>
-        <label for="prix">Prix : </label> en bronze
-        <input type="number" id="prix" name="prix" required min="1" max="5000" value="1"> <br>
-        <label for="description">Description : </label> <br>
-        <textarea name="description" id="description" autofocus cols="40" rows="10" style="resize : none;" required
-            minlength="1" maxlength="80"></textarea><br>
-        <label for="matiere">Matière de l'armure : </label>
-        <input type="text" name="matiere" id="matiere" required minlength="1" maxlength="40"> <br>
-        <label for="taille">Taille en cm :</label>
-        <input type="number" name="taille" id="taille" required min="1" max="60"> cm <br>
-        <label for="quantite">Quantité à ajouter : </label>
-        <input type="number" min="1" max="20" value="1" name="quantite" id="quantite" required> <br>
-        <label for="image">Image :</label><br>
-        <input type="hidden" name="MAX_FILE_SIZE" value="50000000" required>
-        <input type="file" id="image" name="url" accept="image/*" required><br>
-        <input type="submit" value="Ajouter">
-    </form>
-    <form action="ajout.php" method="POST" id="formPotion" style="display: none;padding: 4px;"
-        enctype="multipart/form-data">
-        <input type="hidden" name="type" value="formPotion">
-        <h2>Ajouter une potion</h2>
-        <label for="nom">Nom : </label>
-        <input type="text" name="nom" id="nom" required><br>
-        <label for="prix">Prix : </label>
-        <input type="number" id="prix" name="prix" required min="1" max="5000" value="1"> <br>
-        <label for="description">Description : </label> <br>
-        <textarea name="description" id="description" autofocus cols="40" rows="10" style="resize : none;" required
-            minlength="1" maxlength="80"></textarea><br>
-        <label for="effet">Effet : </label>
-        <input type="text" name="effet" id="effet" required minlength="1" maxlength="60"> <br>
-        <label for="duree">Durée en secondes : </label>
-        <input type="number" id="duree" name="duree" required min="1" max="600"> secondes <br>
-        <label for="quantite">Quantité à ajouter : </label>
-        <input type="number" min="1" max="20" value="1" name="quantite" id="quantite" required> <br>
-        <label for="image">Image :</label><br>
-        <input type="hidden" name="MAX_FILE_SIZE" value="50000000" required>
-        <input type="file" id="image" name="url" accept="image/*" required><br>
-        <input type="submit" value="Ajouter">
-    </form>
-    <form action="ajout.php" method="POST" id="formSort" style="display: none; padding: 4px;"
-        enctype="multipart/form-data">
-        <input type="hidden" name="type" value="formSort">
-        <h2>Ajouter un sort</h2>
-        <label for="nom">Nom : </label>
-        <input type="text" name="nom" id="nom" required><br>
-        <label for="prix">Prix : </label>
-        <input type="number" id="prix" name="prix" required min="1" max="5000" value="1"> <br>
-        <label for="description">Description : </label> <br>
-        <textarea name="description" id="description" autofocus cols="40" rows="10" style="resize : none;" required
-            minlength="1" maxlength="80"></textarea><br>
-        <p>Est-ce que le sort est instantané ?</p>
-        <input type="radio" id="oui" name="instantane" value="1" checked>
-        <label for="oui"> Oui</label>
-        <input type="radio" id="non" name="instantane" value="0">
-        <label for="non">Non</label> <br>
-        <label for="dommage">Dommage du sort : </label>
-        <input type="number" id="dommage" name="dommage" required min="1" max="100"> points de vie<br>
-        <label for="quantite">Quantité à ajouter : </label>
-        <input type="number" min="1" max="20" value="1" name="quantite" id="quantite" required> <br>
-        <label for="image">Image :</label><br>
-        <input type="hidden" name="MAX_FILE_SIZE" value="50000000" required>
-        <input type="file" id="image" name="url" accept="image/*" required><br>
-        <input type="submit" value="Ajouter">
-    </form>
-    <?php
-    if (isset($erreur)) {
-        if (!$erreur) {
-            echo "<span style='color:red'> Erreur dans les données </span>";
+        </form>
+        <form action="ajout.php" method="POST" id="formArmure" style="display: none;padding: 4px;"
+            enctype="multipart/form-data">
+            <input type="hidden" name="type" value="formArmure">
+            <h2>Ajouter une armure</h2>
+            <label for="nom">Nom : </label>
+            <input type="text" name="nom" id="nom" minlength="1" maxlength="80" required><br>
+            <label for="prix">Prix : </label> en bronze
+            <input type="number" id="prix" name="prix" required min="1" max="5000" value="1"> <br>
+            <label for="description">Description : </label> <br>
+            <textarea name="description" id="description" autofocus cols="40" rows="10" style="resize : none;" required
+                minlength="1" maxlength="80"></textarea><br>
+            <label for="matiere">Matière de l'armure : </label>
+            <input type="text" name="matiere" id="matiere" required minlength="1" maxlength="40"> <br>
+            <label for="taille">Taille en cm :</label>
+            <input type="number" name="taille" id="taille" required min="1" max="60"> cm <br>
+            <label for="quantite">Quantité à ajouter : </label>
+            <input type="number" min="1" max="20" value="1" name="quantite" id="quantite" required> <br>
+            <label for="image">Image :</label><br>
+            <input type="hidden" name="MAX_FILE_SIZE" value="50000000" required>
+            <input type="file" id="image" name="url" accept="image/*" required><br>
+            <input type="submit" value="Ajouter">
+        </form>
+        <form action="ajout.php" method="POST" id="formPotion" style="display: none;padding: 4px;"
+            enctype="multipart/form-data">
+            <input type="hidden" name="type" value="formPotion">
+            <h2>Ajouter une potion</h2>
+            <label for="nom">Nom : </label>
+            <input type="text" name="nom" id="nom" required><br>
+            <label for="prix">Prix : </label>
+            <input type="number" id="prix" name="prix" required min="1" max="5000" value="1"> <br>
+            <label for="description">Description : </label> <br>
+            <textarea name="description" id="description" autofocus cols="40" rows="10" style="resize : none;" required
+                minlength="1" maxlength="80"></textarea><br>
+            <label for="effet">Effet : </label>
+            <input type="text" name="effet" id="effet" required minlength="1" maxlength="60"> <br>
+            <label for="duree">Durée en secondes : </label>
+            <input type="number" id="duree" name="duree" required min="1" max="600"> secondes <br>
+            <label for="quantite">Quantité à ajouter : </label>
+            <input type="number" min="1" max="20" value="1" name="quantite" id="quantite" required> <br>
+            <label for="image">Image :</label><br>
+            <input type="hidden" name="MAX_FILE_SIZE" value="50000000" required>
+            <input type="file" id="image" name="url" accept="image/*" required><br>
+            <input type="submit" value="Ajouter">
+        </form>
+        <form action="ajout.php" method="POST" id="formSort" style="display: none; padding: 4px;"
+            enctype="multipart/form-data">
+            <input type="hidden" name="type" value="formSort">
+            <h2>Ajouter un sort</h2>
+            <label for="nom">Nom : </label>
+            <input type="text" name="nom" id="nom" required><br>
+            <label for="prix">Prix : </label>
+            <input type="number" id="prix" name="prix" required min="1" max="5000" value="1"> <br>
+            <label for="description">Description : </label> <br>
+            <textarea name="description" id="description" autofocus cols="40" rows="10" style="resize : none;" required
+                minlength="1" maxlength="80"></textarea><br>
+            <p>Est-ce que le sort est instantané ?</p>
+            <input type="radio" id="oui" name="instantane" value="1" checked>
+            <label for="oui"> Oui</label>
+            <input type="radio" id="non" name="instantane" value="0">
+            <label for="non">Non</label> <br>
+            <label for="dommage">Dommage du sort : </label>
+            <input type="number" id="dommage" name="dommage" required min="1" max="100"> points de vie<br>
+            <label for="quantite">Quantité à ajouter : </label>
+            <input type="number" min="1" max="20" value="1" name="quantite" id="quantite" required> <br>
+            <label for="image">Image :</label><br>
+            <input type="hidden" name="MAX_FILE_SIZE" value="50000000" required>
+            <input type="file" id="image" name="url" accept="image/*" required><br>
+            <input type="submit" value="Ajouter">
+        </form>
+        <?php
+        if (isset($erreur)) {
+            if (!$erreur) {
+                echo "<span style='color:red'> Erreur dans les données </span>";
+            }
         }
-    }
-    ?>
-
+        ?>
+    </div>
 </main>
 
 <?php include 'include/footer.php' ?>
--- ajoutEnigma.php
+++ ajoutEnigma.php
@@ -20,7 +20,7 @@ if ($_SERVER['REQUEST_METHOD'] === "POST" && $_POST['type'] === "formEnigme") {
     ) {
 
         $reponses = [];
-        $reponses[] = $_POST['correct']; 
+        $reponses[] = $_POST['correct'];
 
         foreach ($_POST['faux'] as $f) {
             $reponses[] = $f;
@@ -42,6 +42,8 @@ if ($_SERVER['REQUEST_METHOD'] === "POST" && $_POST['type'] === "formEnigme") {
 }
 
 ?>
+
+<link rel="stylesheet" href="public/css/enigma.css">
 <title>DarQuest - Ajout Énigma</title>
 
 <?php
@@ -50,54 +52,56 @@ include 'include/nav.php';
 ?>
 
 <main class="main">
-    <div class="type-buttons">
-        <button type="button" onclick="Form()">Ajouter une énigme</button>
+    <div class="enigmaAjout-container">
+        <div class="type-buttons">
+            <button type="button" onclick="Form()">Ajouter une énigme</button>
+        </div>
+
+        <form action="ajoutEnigma.php" method="POST" id="formEnigme" enctype="multipart/form-data"
+            style="display: none; padding: 4px;">
+            <input type="hidden" name="type" value="formEnigme">
+            <h2>Ajouter une énigme</h2>
+            <label for="enonce">Énoncé : </label> <br>
+            <textarea name="enonce" id="enonce" autofocus cols="40" rows="10" style="resize : none;"
+                required></textarea><br>
+            <label for="categorie">Catégorie :</label>
+            <select id="categorie" name="categorie" required>
+                <option value="" disabled selected>Choisir une catégorie</option>
+                <option value="armure">Armure</option>
+                <option value="arme">Arme</option>
+                <option value="potion">Potion</option>
+                <option value="sort">Sort</option>
+                <option value="magie">Magie</option>
+                <option value="monde">Monde</option>
+            </select><br>
+            <label>Réponses :</label><br>
+
+            <div class="reponse-block">
+                <input type="text" name="correct" placeholder="Bonne réponse" required>
+            </div><br>
+
+            <div class="reponse-block">
+                <input type="text" name="faux[]" placeholder="Mauvaise réponse 1" required>
+            </div><br>
+
+            <div class="reponse-block">
+                <input type="text" name="faux[]" placeholder="Mauvaise réponse 2" required>
+            </div> <br>
+
+            <div class="reponse-block">
+                <input type="text" name="faux[]" placeholder="Mauvaise réponse 3" required>
+            </div><br>
+
+            <p>Difficulté : </p>
+            <label for="facile"> Facile </label> 
+            <input type="radio" id="facile" name="difficulte" value="f" checked>
+            <label for="moyenne">Moyenne </label> 
+            <input type="radio" id="moyenne" name="difficulte" value="m">
+            <label for="difficile">Difficile </label> 
+            <input type="radio" id="difficile" name="difficulte" value="d"><br>
+            <input type="submit" value="Ajouter">
+        </form>
     </div>
-
-    <form action="ajoutEnigma.php" method="POST" id="formEnigme" enctype="multipart/form-data"
-        style="display: none; padding: 4px;">
-        <input type="hidden" name="type" value="formEnigme">
-        <h2>Ajouter une énigme</h2>
-        <label for="enonce">Énoncé : </label> <br>
-        <textarea name="enonce" id="enonce" autofocus cols="40" rows="10" style="resize : none;"
-            required></textarea><br>
-        <label for="categorie">Catégorie :</label>
-        <select id="categorie" name="categorie" required>
-            <option value="" disabled selected>Choisir une catégorie</option>
-            <option value="armure">Armure</option>
-            <option value="arme">Arme</option>
-            <option value="potion">Potion</option>
-            <option value="sort">Sort</option>
-            <option value="magie">Magie</option>
-            <option value="monde">Monde</option>
-        </select><br>
-        <label>Réponses :</label><br>
-
-        <div class="reponse-block">
-            <input type="text" name="correct" placeholder="Bonne réponse" required>
-        </div><br>
-
-        <div class="reponse-block">
-            <input type="text" name="faux[]" placeholder="Mauvaise réponse 1" required>
-        </div><br>
-
-        <div class="reponse-block">
-            <input type="text" name="faux[]" placeholder="Mauvaise réponse 2" required>
-        </div> <br>
-
-        <div class="reponse-block">
-            <input type="text" name="faux[]" placeholder="Mauvaise réponse 3" required>
-        </div><br>
-
-        <p>Difficulté ?</p>
-        <input type="radio" id="facile" name="difficulte" value="f" checked>
-        <label for="facile"> Facile </label> <br>
-        <input type="radio" id="moyenne" name="difficulte" value="m">
-        <label for="moyenne">Moyenne </label> <br>
-        <input type="radio" id="difficile" name="difficulte" value="d">
-        <label for="difficile">Difficile </label> <br>
-        <input type="submit" value="Ajouter">
-    </form>
 </main>
 
 <script>
